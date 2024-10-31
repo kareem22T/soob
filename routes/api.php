3e98bf7fd83d956252c57570ca4524e5f7cb2968
kareem22T <m.othman@clicksegypt.net>
2024-10-31 05:38:24 +0300

    first

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\API\BookingController;
 use App\Http\Controllers\API\OfferController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\UserCustomeRequestController;
